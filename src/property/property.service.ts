6d7d37e6a1ee44bd1d0a69c59d80a83369efa3c0
unknown <mr7aali@gmail.com>
2025-08-12 13:04:18 +0600

    a

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Property, PropertyDocument } from './schemas/property.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class PropertyService {}
+export class PropertyService {
+  constructor(
+    @InjectModel(Property.name) private propertyModel: Model<PropertyDocument>,
+  ) {}
+  async create(data: Property) {
+    return await this.propertyModel.create(data);
+  }
+}

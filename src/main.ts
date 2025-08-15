7e444669d24227f5cf094055243497f54b755eb0
unknown <mr7aali@gmail.com>
2025-08-15 21:02:11 +0600

    cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import cookieParser from 'cookie-parser';
-// import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  //   credentials: true,
-  // });
+
   app.enableCors({
     origin: [
       'http://localhost:3000',
@@ -34,7 +30,6 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new AllExceptionsFilter());
-  // API prefix
   app.setGlobalPrefix('api/v1');
   const port = process.env.PORT ?? 5000;
   await app.listen(port);
